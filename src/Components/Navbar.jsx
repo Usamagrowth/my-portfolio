2ed7d37b5b06d5f273e1ec42b414062918dbc4f8
Usama <usamaomodara@gmail.com>
2025-10-23 15:46:55 +0100

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,18 +10,19 @@ const mobileNav = () => {
 }
 
   return (
-    <div className="px-10 bg-[#004600] w-[100%] fixed" > 
-     <nav className="flex justify-between h-[110px] items-center">
-    <div className="w-[80px] h-[80px]">
+    <div id="home" className="px-10  bg-[#004600] w-[100%] fixed" > 
+     <nav className="flex justify-between h-[110px] items-center ">
+     <div className="w-[80px] h-[80px]">
         <img className="w-[100%] h-[100%] rounded-xl" src="favicon.jpg" alt="Best Web Designer and Development, Digital Marketing, Front End Developer, SEO Experts, Leads Generation, Logo and Flyers Designs in Ibadan Nigerial" />
       </div>
       <div onClick={mobileNav} className="lg:hidden cursor-pointer">
-       {mobile  ? <X  className="text-white" /> :
+       {mobile  ? 
+      <X  className="text-white absolute top-6 right-6" /> :
       <Menu className="text-white" />}
-      </div>
-      <div className="hidden lg:flex">
+      </div> 
+        <div className="hidden lg:flex">
         <ul className="flex gap-9">
-           <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Home</li>
+          <a href="#home"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Home</li></a>
          <a href="#about"> <li className="cursor-pointer text-l font-semibold text-white hover:text-gray-400">About</li></a>
          <a href="#service"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Service</li></a>
          <a href="#portfolio"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Portfolio</li></a>
@@ -30,22 +31,21 @@ const mobileNav = () => {
         </ul>
       </div>
      </nav>
-     
+  
      { mobile && 
      <aside>
-     <div className="lg:hidden py-5 flex">
-      <ul className="flex flex-col gap-5">
-          <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Home</li>
-          <a href="#about"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">About</li></a>
-         <a href="#service"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Service</li></a>
-         <a href="#portfolio"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Portfolio</li></a>
-        <a href="https://usamagrowthsolutions.blogspot.com/">  <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Blog</li></a>
-        <a href="#contact">  <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Contact</li></a>
+     <div className="lg:hidden py-5 flex ">
+      <ul className="flex flex-col gap-5 ">
+         <a onClick={()=> setMobile(false)} href="#home"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Home</li></a>
+          <a onClick={()=> setMobile(false)} href="#about"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">About</li></a>
+         <a onClick={mobileNav} href="#service"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Service</li></a>
+         <a onClick={mobileNav} href="#portfolio"> <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Portfolio</li></a>
+        <a onClick={mobileNav} href="https://usamagrowthsolutions.blogspot.com/">  <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Blog</li></a>
+        <a onClick={()=> setMobile(false)} href="#contact">  <li className="cursor-pointer text-l font-semibold  text-white hover:text-gray-400">Contact</li></a>
         </ul>
      </div>
      </aside>
      }
-  
     </div>
   )
 }
