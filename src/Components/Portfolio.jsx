7e3739b98ae31e633033846ee58a216f1c845a8e
Usama <usamaomodara@gmail.com>
2025-11-03 14:05:36 +0100

    portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = () => {
        image={voyager}
        description="A website build for Traveling, Tours and Hotel Booking to make travelling easier.
        Tech Stack: React.jsx | Vite | Tailwind CSS | JS"
-       link="https://travelandtourvoyage.com" 
+       link="https://travelsandtourvoyage.com" 
        Live="Live Site"
        git="https://github.com/Usamagrowth/Voyager-travelling"
        Github="Github"
